bd08991e0e2dd82d8d9d2a346e1724ffcf46ecd3
CrimeMoot <wakeafa@gmail.com>
2026-01-14 23:52:50 +0300

    do revert

--- Content.Shared/Eye/Blinding/Components/EyeProtectionComponent.cs
+++ Content.Shared/Eye/Blinding/Components/EyeProtectionComponent.cs
@@ -3,7 +3,7 @@ namespace Content.Shared.Eye.Blinding.Components;
 /// <summary>
 /// For welding masks, sunglasses, etc.
 /// </summary>
-[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+[RegisterComponent]
 public sealed partial class EyeProtectionComponent : Component
 {
     /// <summary>
