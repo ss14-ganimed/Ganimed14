535995fdc173c0874e18707fc9d8bab71d389d67
Yuoko <128321596+Yuoko@users.noreply.github.com>
2026-03-16 13:15:22 +0700

    Making the requested changes

--- Content.Shared/Silicons/StationAi/Systems/SharedStationAiSystem.cs
+++ Content.Shared/Silicons/StationAi/Systems/SharedStationAiSystem.cs
@@ -34,7 +34,7 @@ using Robust.Shared.Serialization;
 using Robust.Shared.Timing;
 using Robust.Shared.Utility;
 using Content.Shared.GameTicking;
-using Content.Shared.Examine;
+using Content.Shared.Examine; // Ganimed-Add
 
 namespace Content.Shared.Silicons.StationAi;
 
@@ -116,7 +116,7 @@ public abstract partial class SharedStationAiSystem : EntitySystem
         SubscribeLocalEvent<StationAiCoreComponent, BreakageEventArgs>(OnBroken);
         SubscribeLocalEvent<StationAiCoreComponent, RepairedEvent>(OnRepaired);
 
-        SubscribeLocalEvent<StationAiWhitelistComponent, ExaminedEvent>(OnExamined);
+        SubscribeLocalEvent<StationAiWhitelistComponent, ExaminedEvent>(OnExamined); // Ganimed-Add
     }
 
     private void OnCoreVerbs(Entity<StationAiCoreComponent> ent, ref GetVerbsEvent<Verb> args)
@@ -392,6 +392,7 @@ public abstract partial class SharedStationAiSystem : EntitySystem
             _appearance.SetData(ent, StationAiVisuals.Broken, false, appearance);
     }
 
+    // Ganimed-Add-Start
     private void OnExamined(EntityUid uid, StationAiWhitelistComponent component, ExaminedEvent args)
     {
         if (!component.Enabled)
@@ -399,6 +400,7 @@ public abstract partial class SharedStationAiSystem : EntitySystem
 
         args.PushMarkup(Loc.GetString("station-ai-whitelist-examine"));
     }
+    // Ganimed-Add-End
 
     public virtual void KillHeldAi(Entity<StationAiCoreComponent> ent)
     {
