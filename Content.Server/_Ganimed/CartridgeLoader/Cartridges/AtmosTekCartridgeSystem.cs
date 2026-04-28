df2326f028f964e071a83b6ecf3e6fce31fcb0f1
Hyper B <137433177+HyperB1@users.noreply.github.com>
2026-04-28 08:14:06 +0500

    Gas analyzer cartridge
    AtmosTek Cartridge

---
+++ Content.Server/_Ganimed/CartridgeLoader/Cartridges/AtmosTekCartridgeSystem.cs
@@ -0,0 +1,33 @@
+using Content.Shared.Atmos.Components;
+using Content.Shared.CartridgeLoader;
+using Content.Server.Atmos.Components;
+using Content.Server.CartridgeLoader;
+
+namespace Content.Server._Ganimed.CartridgeLoader.Cartridges;
+
+public sealed class AtmosTekCartridgeSystem : EntitySystem
+{
+    [Dependency] private readonly CartridgeLoaderSystem _cartridgeLoaderSystem = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<AtmosTekCartridgeComponent, CartridgeAddedEvent>(OnCartridgeAdded);
+        SubscribeLocalEvent<AtmosTekCartridgeComponent, CartridgeRemovedEvent>(OnCartridgeRemoved);
+    }
+
+    private void OnCartridgeAdded(Entity<AtmosTekCartridgeComponent> ent, ref CartridgeAddedEvent args)
+    {
+        var gasAnalyzer = EnsureComp<GasAnalyzerComponent>(args.Loader);
+    }
+
+    private void OnCartridgeRemoved(Entity<AtmosTekCartridgeComponent> ent, ref CartridgeRemovedEvent args)
+    {
+        // only remove when the program itself is removed
+        if (!_cartridgeLoaderSystem.HasProgram<AtmosTekCartridgeComponent>(args.Loader))
+        {
+            RemComp<GasAnalyzerComponent>(args.Loader);
+        }
+    }
+}
