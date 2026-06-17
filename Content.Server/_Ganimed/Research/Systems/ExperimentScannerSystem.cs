e952cc3f93608d8cd048b6b948f29e35ddacf00b
Hyper B <137433177+HyperB1@users.noreply.github.com>
2026-06-17 14:52:07 +0500

    Add headers to new files

--- Content.Server/_Ganimed/Research/Systems/ExperimentScannerSystem.cs
+++ Content.Server/_Ganimed/Research/Systems/ExperimentScannerSystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2026 Gorox221 <139872389+Gorox221@users.noreply.github.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using System.Linq;
 using Content.Server.Administration.Logs;
 using Content.Server._Ganimed.Research.Components;
